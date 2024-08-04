8dc7920be8b47b86879faa1188923aca2cb8f0c9
MrPrasetyo <prasetyo1103id@gmail.com>
2024-08-04 21:44:08 +0700

    penambahan detail di table

--- src/components/TableCompo.jsx
+++ src/components/TableCompo.jsx
@@ -5,9 +5,26 @@ import Spinner from "./Spinner";
 import SkeletonTable from "./SkeletonTable";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { FaRegStar, FaStar } from "react-icons/fa6";
+import { FaStarHalfAlt } from "react-icons/fa";
 
-const TABLE_HEAD = ["Photo", "Name Products", "Category", "Price", "Actions"];
+const TABLE_HEAD = ["Photo", "Name Products", "Category", "Price", "Rating", "Actions"];
 const ITEMS_PER_PAGE = 6;
+
+const getCategoryBackgroundColor = (category) => {
+  switch (category) {
+    case "beauty":
+      return "bg-green-500";
+    case "fragrances":
+      return "bg-pink-400";
+    case "furniture":
+      return "bg-amber-700";
+    case "groceries":
+      return "bg-lime-500";
+    default:
+      return "bg-gray-200";
+  }
+};
 
 export function TableCompo() {
   const [store, setStore] = useState([]);
@@ -60,7 +77,7 @@ export function TableCompo() {
           icon={<MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />}
         />
       </div>
-      <table className="w-full min-w-max table-auto text-left relative">
+      <table className="w-full table-auto text-left relative">
         <thead>
           <tr>
             {TABLE_HEAD.map((head) => (
@@ -81,27 +98,35 @@ export function TableCompo() {
               const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
               return (
                 <tr key={product.id}>
-                  <td className={classes}>
+                  <td className={`${classes} w-24`}>
                     <div className="flex items-center gap-3">
                       <Avatar src={product.thumbnail} alt={product.title} size="md" className="border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1" />
                     </div>
                   </td>
-                  <td className={classes}>
-                    <Typography variant="small" color="blue-gray" className="font-normal">
-                      {product.title}
-                    </Typography>
+                  <td className={`${classes} w-96`}>
+                    <div className="flex flex-col">
+                      <Typography variant="h6" color="blue-gray" className="text-md">{product.title}</Typography>
+                      <Typography variant="small" className="text-sm font-normal text-gray-500">{product.brand}</Typography>
+                    </div>
+                    
                   </td>
-                  <td className={classes}>
-                    <Typography variant="small" color="blue-gray" className="font-normal">
+                  <td className={`${classes} w-44`}>
+                    <Typography variant="small" className={`font-normal px-2 py-1 rounded-full w-fit h-fit text-white ${getCategoryBackgroundColor(product.category)}`}>
                       {product.category}
                     </Typography>
                   </td>
-                  <td className={classes}>
-                    <Typography variant="small" color="blue-gray" className="font-normal">
-                      $ {product.price}
-                    </Typography>
+                  <td className={`${classes} w-40`}>
+                    <div className="flex flex-col">
+                      <Typography className="font-bold text-red-500 text-md tracking-tight"><span className="text-gray-900 font-thin">$ </span>{product.price}</Typography>
+                      <Typography className="text-xs text-gray-500">stock : {product.stock}</Typography>
+                    </div>
                   </td>
-                  <td className={classes}>
+                  <td className={`${classes}`}>
+                  <Typography className="flex items-center">
+                    <FaStar className="text-yellow-500"/><span className="ml-2">{product.rating}</span>
+                  </Typography>
+                  </td>
+                  <td className={`${classes} w-24`}>
                     <Tooltip content="Edit product">
                       <IconButton variant="text">
                         <PencilIcon className="h-4 w-4" />
